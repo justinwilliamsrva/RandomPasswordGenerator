d49d41f084b4b24903d575c2df0bff68fda5d3b8
justinwilliamsrva <justin.williams2@me.com>
2020-06-10 12:20:28 -0400

    Added a Slider so the user can select 8-128 items

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+slider.oninput = function () {
+  output.innerHTML = this.value;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
